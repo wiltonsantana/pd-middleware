e22ab58c2c684d2414871cf3f48805b1b4575b70
Wilton Santana <wpss@cin.ufpe.br>
2019-05-05 19:44:03 -0300

    Marshaller: Add Unmarshall method
    This patch add unmarshall method in order to allow users to deserialize
    incoming messages.

--- cmd/middleware/distribution/marshaller.go
+++ cmd/middleware/distribution/marshaller.go
@@ -11,7 +11,14 @@ func NewMarshaller() Marshaller {
 	return Marshaller{}
 }
 
-// Marshal maps the message to JSON e serialize it in bytes.
+// Marshal maps the message to JSON and serialize it in bytes.
 func (marshaller *Marshaller) Marshal(message Message) ([]byte, error) {
 	return json.Marshal(message)
+}
+
+// Unmarshal deserialize bytes message and maps it to JSON.
+func (marshaller *Marshaller) Unmarshal(message []byte) (*Message, error) {
+	msg := Message{}
+	err := json.Unmarshal(message, &msg)
+	return &msg, err
 }
